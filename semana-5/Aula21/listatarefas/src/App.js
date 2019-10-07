784b1fb19c038b221f63139b27ad339751886bbb
gamedesignbrasil <thiago@spellbook.com.br>
2019-10-07 16:35:01 -0300

    novas ideias, mas ainda fail

--- semana-5/Aula21/listatarefas/src/App.js
+++ semana-5/Aula21/listatarefas/src/App.js
@@ -9,30 +9,50 @@ class App extends Component {
     this.state = {
       items: [],
       currentItem: {
-        text: '',
-        key: '',
+        text: "",
+        key: "",
+        status: "",
       },
     }
   }
 
-  
+  // Deletar o item de acordo com a key passada - ok
+
+  // A ideia é mudar isso aqui pra um map (?), daí todo item deletado ganha o status OFF e todo item ativo ganha status ON
+  // Ao clicar, muda esse status de ON pra OFF
+  // Os itens ON são mostrados na toDo list e os itens OFF são monstrados na lista de tarefas concluídas
+  // Obviamente ainda não sei fazer, mas a ideia faz sentido, haha
   deleteItem = key => {
     const filteredItems = this.state.items.filter(item => {
       return item.key !== key
     })
-    this.setState({
+
+    this.setState(
+      {
       items: filteredItems,
-    })
+      }
+    )
   }
 
 
   // Salva o valor do input + uma maneira legal de lidar com a key ensinado pelo stackoverflow :)
   handleInput = e => {
     const itemText = e.target.value
-    const currentItem = { text: itemText, key: Date.now() }
+    const currentItem = { text: itemText, key: Date.now(), status: "on" }
     this.setState({
       currentItem,
     })
+  }
+
+  addItem = e => {
+    const newItem = this.state.currentItem
+    if (newItem.text !== '') {
+      const items = [...this.state.items, newItem]
+      this.setState({
+        items: items,
+        currentItem: { text: "", key: "" },
+      })
+    }
   }
 
 
@@ -40,12 +60,19 @@ class App extends Component {
   addItem = e => {
     e.preventDefault()
     const newItem = this.state.currentItem
+
     if (newItem.text !== '') {
+
       const items = [...this.state.items, newItem]
-      this.setState({
+      console.log(items)
+
+      this.setState(
+        {
         items: items,
-        currentItem: { text: '', key: '' },
-      })
+        currentItem: { text: "", key: "", status: "" },
+        }
+      )
+
     }
   }
 
