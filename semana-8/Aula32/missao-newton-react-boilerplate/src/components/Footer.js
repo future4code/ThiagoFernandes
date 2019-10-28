8e04fd455335e090634624a4b0233dd758842874
gamedesignbrasil <thiago@spellbook.com.br>
2019-10-28 15:33:29 -0300

    Todo list

---
+++ semana-8/Aula32/missao-newton-react-boilerplate/src/components/Footer.js
@@ -0,0 +1,26 @@
+/* eslint-disable linebreak-style */
+import React from 'react'
+
+
+export class Footer extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+		}
+	}
+
+
+	render() {
+		return (
+			<div>
+				<div>
+					<button>Completar Todas</button>
+					<button>Ver completas</button>
+					<button>Ver pendentes</button>
+				</div>
+
+
+			</div>
+		);
+	}
+}
