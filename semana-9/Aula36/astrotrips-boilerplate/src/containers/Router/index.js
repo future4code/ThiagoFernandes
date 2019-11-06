d2fd830928d7ccd009e9b694e0c42dddc3b58b6f
gamedesignbrasil <thiago@spellbook.com.br>
2019-11-06 16:31:04 -0300

    Novo container e começo do axios

--- semana-9/Aula36/astrotrips-boilerplate/src/containers/Router/index.js
+++ semana-9/Aula36/astrotrips-boilerplate/src/containers/Router/index.js
@@ -4,11 +4,13 @@ import { Switch, Route } from "react-router-dom";
 import LoginPage from "../LoginPage";
 import HomePage from "../HomePage";
 import AppForm from "../AppForm";
+import ListTripsPage from "../ListTripsPage";
 
 const routes = {
   root: "/",
   login: "/login",
-  cadastrar: "/application-form"
+  cadastrar: "/application-form",
+  listaviagens: "/trips/list"
   // Outras rotas aqui
 };
 
@@ -16,6 +18,7 @@ function Router(props) {
   return (
     <ConnectedRouter history={props.history}>
       <Switch>
+      <Route path={routes.listaviagens} component={ListTripsPage} />
         <Route path={routes.login} component={LoginPage} />
         <Route path={routes.cadastrar} component={AppForm} />
         <Route path={routes.root} component={HomePage} />
