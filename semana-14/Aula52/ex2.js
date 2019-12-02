3cb3c125816d7bf11776a3f3a4725a5ac0ac1ffb
gamedesignbrasil <thiago@spellbook.com.br>
2019-12-02 13:15:22 -0300

    Added colors to ex2

--- semana-14/Aula52/ex2.js
+++ semana-14/Aula52/ex2.js
@@ -4,7 +4,7 @@ const newTask = "\n" + process.argv[3];
 
 try {
 	fs.appendFileSync(allTasks, newTask, "utf8");
-	console.log("Tarefa adicionada com sucesso");
+	console.log("\x1b[32m%s\x1b[0m", "Tarefa adicionada com sucesso!");
 } catch(err){
-	console.log(err)
+	console.log("\x1b[31m%s\x1b[0m", err)
 }
