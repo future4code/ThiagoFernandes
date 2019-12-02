4b2289541dd0583a9723cb96a7252d8616663598
gamedesignbrasil <thiago@spellbook.com.br>
2019-12-02 13:13:41 -0300

    Added colors to ex1

--- semana-14/Aula52/ex1.js
+++ semana-14/Aula52/ex1.js
@@ -2,12 +2,19 @@ const operacao = process.argv[2];
 const valor1 = Number(process.argv[3]);
 const valor2 = Number(process.argv[4]);
 
+
 if (operacao === "add"){
-	console.log(valor1 + valor2);
-} else if (operacao === "sub"){
-	console.log(valor1 - valor2);
-} else if (operacao === "div"){
-	console.log(valor1 / valor2);
-} else {
-	console.log(valor1 * valor2);
+	console.log("\x1b[32m%s\x1b[0m", "\nA resposta é: " + (valor1 + valor2));
+	} else if (operacao === "sub"){
+	console.log("\x1b[32m%s\x1b[0m", "\nA resposta é: " + (valor1 - valor2));
+	} else if (operacao === "div"){
+	console.log("\x1b[32m%s\x1b[0m", "\nA resposta é: " + (valor1 / valor2));
+	} else if (operacao === "mult") {
+	console.log("\x1b[32m%s\x1b[0m", "\nA resposta é: " + (valor1 * valor2));
+	} else {
+	console.log("\x1b[31m%s\x1b[0m", "Operação inválida. Escolha entre ADD, SUB, DIV ou MULT.");
 }
+
+
+
+
