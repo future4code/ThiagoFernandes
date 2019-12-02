c9411ef8ea4470112bf1db56f571f24264ce6ff1
gamedesignbrasil <thiago@spellbook.com.br>
2019-12-02 13:40:54 -0300

    Added input verification to ex1

--- semana-14/Aula52/ex1.js
+++ semana-14/Aula52/ex1.js
@@ -2,19 +2,30 @@ const operacao = process.argv[2];
 const valor1 = Number(process.argv[3]);
 const valor2 = Number(process.argv[4]);
 
-
 if (operacao === "add"){
-	console.log("\x1b[32m%s\x1b[0m", "\nA resposta é: " + (valor1 + valor2));
-	} else if (operacao === "sub"){
-	console.log("\x1b[32m%s\x1b[0m", "\nA resposta é: " + (valor1 - valor2));
-	} else if (operacao === "div"){
-	console.log("\x1b[32m%s\x1b[0m", "\nA resposta é: " + (valor1 / valor2));
-	} else if (operacao === "mult") {
-	console.log("\x1b[32m%s\x1b[0m", "\nA resposta é: " + (valor1 * valor2));
+	if (isNaN(valor1) || isNaN(valor2)){
+		console.log("\x1b[31m%s\x1b[0m", "Erro. Favor inserir DOIS valores (ambos devem ser numéricos).")
 	} else {
+		console.log("\x1b[32m%s\x1b[0m", "\nA resposta é: " + (valor1 + valor2));
+	}
+} else if (operacao === "sub"){
+		if (isNaN(valor1) || isNaN(valor2)){
+		console.log("\x1b[31m%s\x1b[0m", "Erro. Favor inserir DOIS valores e ambos devem ser numéricos.");
+		} else {
+			console.log("\x1b[32m%s\x1b[0m", "\nA resposta é: " + (valor1 - valor2));
+		}
+} else if (operacao === "div"){
+	if (isNaN(valor1) || isNaN(valor2)){
+		console.log("\x1b[31m%s\x1b[0m", "Erro. Favor inserir DOIS valores (ambos devem ser numéricos).")
+	} else {
+		console.log("\x1b[32m%s\x1b[0m", "\nA resposta é: " + (valor1 / valor2));
+	}
+} else if (operacao === "mult") {
+	if (isNaN(valor1) || isNaN(valor2)){
+		console.log("\x1b[31m%s\x1b[0m", "Erro. Favor inserir DOIS valores (ambos devem ser numéricos).")
+	} else {
+	console.log("\x1b[32m%s\x1b[0m", "\nA resposta é: " + (valor1 * valor2));
+	}
+} else {
 	console.log("\x1b[31m%s\x1b[0m", "Operação inválida. Escolha entre ADD, SUB, DIV ou MULT.");
 }
-
-
-
-
