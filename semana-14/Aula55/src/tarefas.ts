c4c3c7e2f3350146f8da451761d5945db0dea43e
gamedesignbrasil <thiago@spellbook.com.br>
2019-12-05 16:31:44 -0300

    Adicionando tarefas ao array de tarefas

--- semana-14/Aula55/src/tarefas.ts
+++ semana-14/Aula55/src/tarefas.ts
@@ -8,11 +8,16 @@ const today = moment();
 
 const listAllTasks: string[] = [];
 
-//TODO organizar essa zona: ao cadastrar nova tarefa, a newTask deve ser adiciona a um array de tarefas listAllTasks
-
+//TODO ok mas como trabalhar com agendamento e datas agora?
+// isso agenda a tarefa pro dia e horário em que ela foi cadastrada
 try {
 	appendFileSync(allTasks, newTask, "utf8");
+	listAllTasks.push(newTask);
 	console.log("\x1b[32m%s\x1b[0m", "Tarefa adicionada com sucesso! Agendada para: ", today.utcOffset("-0300").format("DD MM YYYY"));
 } catch(err){
 	console.log("\x1b[31m%s\x1b[0m", err)
 }
+
+console.log("Todas as tarefas:")
+console.log()
+console.log(listAllTasks);
