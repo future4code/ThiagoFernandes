1f016e365f82552893ded82fc1cf1f0ac1e9f795
gamedesignbrasil <thiago@spellbook.com.br>
2019-12-05 16:26:16 -0300

    Organizando

--- semana-14/Aula55/src/tarefas.ts
+++ semana-14/Aula55/src/tarefas.ts
@@ -1,13 +1,17 @@
-import * as moment from "moment"
+import * as moment from "moment";
+import { appendFileSync } from "fs";
 
-const fs = require("fs");
 const allTasks: string = process.argv[2];
 const newTask: string = "\n" + process.argv[3];
 
 const today = moment();
 
+const listAllTasks: string[] = [];
+
+//TODO organizar essa zona: ao cadastrar nova tarefa, a newTask deve ser adiciona a um array de tarefas listAllTasks
+
 try {
-	fs.appendFileSync(allTasks, newTask, "utf8");
+	appendFileSync(allTasks, newTask, "utf8");
 	console.log("\x1b[32m%s\x1b[0m", "Tarefa adicionada com sucesso! Agendada para: ", today.utcOffset("-0300").format("DD MM YYYY"));
 } catch(err){
 	console.log("\x1b[31m%s\x1b[0m", err)
