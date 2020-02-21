d3343c7482a99ef2acad7941f269b1fb4cd53ecb
gamedesignbrasil <thiago@spellbook.com.br>
2020-02-21 09:27:04 -0300

    Aula90

---
+++ semana-23/Aula90/index.js
@@ -0,0 +1,20 @@
+//Ex1 - a
+printNumbersA(n) {
+    if (n >= 0) {
+        printNumbersA(n - 1)
+        console.log(n)
+    }
+}
+
+//Ex1 - b
+printNumbersB(n){
+    if (n >= 0){
+        printNumbersB(n -1)
+        console.log(n)
+    }
+}
+
+//Ex2
+
+
+//Ex3
